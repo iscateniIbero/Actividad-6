4949d28052610466d8c584a5bd3f4a30706909cd
iscateniIbero <87401407+iscateniIbero@users.noreply.github.com>
2022-05-11 07:55:03 -0500

    Actividad 6
    Práctica 1 - Aplicación de la Programación Orientada a Objetos

---
+++ Ejercicio 1.py
@@ -0,0 +1,13 @@
+while True:
+	a = float (input ('Por favor ingrese la base:'))
+	b = float (input ('Por favor ingrese la potencia:'))
+	def power(a,b):
+		s=a**b
+		return s
+	print(a,'^',b,'=',power(a,b))
+	c = int (input ('¿Si desea continuar, escriba 1, de lo contrario escriba 0:  '))
+	if c==0:
+		print ('Programa terminado')
+		break
+	else:
+		continue
---
+++ Ejercicio 4.py
@@ -0,0 +1,7 @@
+string = "Esto es una cadena de texto"
+stringbuffer = bytearray(4)
+stringtokenize = "Esta es una cadena para separar"
+
+poo = stringtokenize.split()
+
+print(poo)
